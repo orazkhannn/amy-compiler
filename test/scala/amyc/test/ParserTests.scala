5c7377713d91195886a842a0974f53cac87507b0
orazkhannn <65410112+orazkhannn@users.noreply.github.com>
2025-06-16 20:49:38 +0800

    Add files via upload

---
+++ test/scala/amyc/test/ParserTests.scala
@@ -0,0 +1,22 @@
+package amyc.test
+
+import amyc.parsing._
+import org.junit.Test
+
+class ParserTests extends TestSuite with amyc.MainHelpers {
+  val pipeline = Lexer andThen Parser andThen treePrinterN("")
+
+  val baseDir = "parser"
+
+  val outputExt = "scala"
+
+  @Test def testLL1 = {
+    assert(Parser.program.isLL1)
+  }
+
+  @Test def testEmpty = shouldOutput("Empty")
+  @Test def testLiterals = shouldOutput("Literals")
+
+  @Test def testEmptyFile = shouldFail("EmptyFile")
+}
+
